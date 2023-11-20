cc81a8a2208ae7de8ac62166acd667cb111ac80a
IconProg <big.balls@list.ru>
2023-11-20 17:49:02 +0300

    #1 Navigation and start Pages

---
+++ app/src/main/java/com/example/harddisks/AuthPages/Constant.java
@@ -0,0 +1,5 @@
+package com.example.harddisks.AuthPages;
+
+public interface Constant {
+    public static final GreetingMain MAIN = new GreetingMain();
+}
---
+++ app/src/main/java/com/example/harddisks/AuthPages/GreetingMain.java
@@ -0,0 +1,4 @@
+package com.example.harddisks.AuthPages;
+
+public class GreetingMain {
+}
